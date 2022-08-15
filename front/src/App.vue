876d79d985bfb6e102fcf0107820d97f4a01d6d7
SOPHAL.SARARN <SOPHAL.SARARN@pnc.lan>
2022-08-15 16:07:07 +0700

    #9 create leave request form

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Addmins</router-link> |
+    <router-link to="/student">Students</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ front/src/views/AddminView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="addmin">
+<h2>Addmin Page</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  components: {
+  }
+}
+</script>
