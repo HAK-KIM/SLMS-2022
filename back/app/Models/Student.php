8aea4000e769786333621cc6e95673c54431427b
unknown <hak.kim@student.passerellesnumeriques.org>
2022-08-16 14:42:21 +0700

    #8 create leave request

---
+++ back/app/Models/Student.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'leave_type',
+        'date_start',
+        'end_start',
+        'status',
+    ];
+
+    protected $casts =  [
+        'status' => 'boolean',
+        'created_at'  => 'datetime:d D M Y H:i:s A',
+        'updated_at' => 'datetime:d D M Y H:i:s A'
+    ];
+}
