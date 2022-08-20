a4edc79fa376603951375fd94cf37e2095e7424d
HAK-KIM <hak.kim@student.passerellesnumeriques.org>
2022-08-20 16:14:57 +0700

    #15 send email

--- back/routes/api.php
+++ back/routes/api.php
@@ -11,3 +11,9 @@ use App\Http\Controllers\LeaveController;
 Route::get('/requests', [LeaveController::class, 'index']);
 Route::post('/requests', [LeaveController::class, 'store']);
 Route::put('/requests/{id}', [LeaveController::class, 'update']);
+
+Route::get('/users', [UserController::class, 'index']);
+
+// single users
+Route::post('user/email/{id}', [UserController::class, 'storeSingleEmail'])->name('store.user.email');
+
